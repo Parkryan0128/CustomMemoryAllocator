04ac770f9eb16051ed668f144f52d7bae3acdb02
jiwonpark <vazywon@hotmail.com>
2025-09-30 22:16:51 -0700

    added benchmark for memory

--- src/FixedSizeAllocator.cpp
+++ src/FixedSizeAllocator.cpp
@@ -6,33 +6,33 @@
 constexpr size_t CHUNK_SIZE = 64 * 1024;
 
 FixedSizeAllocator::FixedSizeAllocator(size_t blockSize) : m_blockSize(blockSize) {
-    std::cout << "Creating an allocator for " << m_blockSize << " byte blocks." << std::endl;
+    // std::cout << "Creating an allocator for " << m_blockSize << " byte blocks." << std::endl;
 
     // ✅ Mission: Allocate a memory chunk from the OS.
     m_chunkStart = alloc_chunk(CHUNK_SIZE);
     
     if (m_chunkStart) {
         m_chunkSize = CHUNK_SIZE; // Store the size only on successful allocation.
-        std::cout << "Successfully acquired " << m_chunkSize << " bytes from OS at address " << m_chunkStart << std::endl;
+        // std::cout << "Successfully acquired " << m_chunkSize << " bytes from OS at address " << m_chunkStart << std::endl;
     } else {
-        std::cerr << "FATAL: Failed to acquire memory from OS. Allocator is unusable." << std::endl;
+        // std::cerr << "FATAL: Failed to acquire memory from OS. Allocator is unusable." << std::endl;
         m_chunkSize = 0;
     }
 }
 
 FixedSizeAllocator::~FixedSizeAllocator() {
-    std::cout << "Destroying allocator. Returning " << m_chunkSize << " bytes to OS." << std::endl;
+    // std::cout << "Destroying allocator. Returning " << m_chunkSize << " bytes to OS." << std::endl;
     // ✅ Mission: Return the allocated memory chunk to the OS.
     free_chunk(m_chunkStart, m_chunkSize);
 }
 
 void* FixedSizeAllocator::allocate() {
     // TODO: To be implemented in the next mission.
-    std::cout << "Allocating " << m_blockSize << " bytes..." << std::endl;
+    // std::cout << "Allocating " << m_blockSize << " bytes..." << std::endl;
     return nullptr;
 }
 
 void FixedSizeAllocator::deallocate(void* ptr) {
     // TODO: To be implemented in the next mission.
-    std::cout << "Deallocating " << m_blockSize << " bytes..." << std::endl;
+    // std::cout << "Deallocating " << m_blockSize << " bytes..." << std::endl;
 }
